b2a037c096bd34d53d071740f98e54dee03fb0af
Rohan Lenka <rohanlenka999@gmail.com>
2025-08-06 22:56:32 +0530

    fixed gap errors and twitter icon

--- src/icons/twitter.tsx
+++ src/icons/twitter.tsx
@@ -1,5 +1,12 @@
+'use client'
+
+import { IconContext } from "react-icons";
+import { FaXTwitter } from "react-icons/fa6";
+
 export default function Twitter() {
-    return <div style={{ height: "20px", width: "20px", backgroundColor: "white", borderRadius: "50%", padding: "5.5px" }}>
-        <img style={{ width: "100%", height: "100%", objectFit: "cover" }} src="/socials/twitter.png" alt="" />
+    return <div style={{ height: "20px", width: "20px", backgroundColor: "white", borderRadius: "50%", padding: "5.5px", textAlign: "center", color: "black" }}>
+        <IconContext.Provider value={{size:'20px'}}>
+            <FaXTwitter />
+        </IconContext.Provider>
     </div>
 }
